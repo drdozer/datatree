a2f010a75c9717633225896937dc31a715dc391b
Matthew Pocock <turingatemyhamster@gmail.com>
2014-12-11 15:36:59 +0000

    Trying to get bintray publishing working.

--- project/DatatreeBuild.scala
+++ project/DatatreeBuild.scala
@@ -24,6 +24,8 @@ object DatatreeBuild extends Build{
     scalacOptions ++= Seq("-deprecation", "-unchecked"),
     version := makeVersion(baseVersion),
     publishMavenStyle := false,
+    bintray.Keys.repository in bintray.Keys.bintray := "sbt-plugins",
+    bintray.Keys.bintrayOrganization in bintray.Keys.bintray := None,
     licenses +=("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0.html"))
   )
 
